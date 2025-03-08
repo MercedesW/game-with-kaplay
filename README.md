e22a485c969b8b992cedc986db5c568319674a99
MercedesW <mercedesw13@protonmail.com>
2025-03-08 17:19:46 -0300

    add files with all code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# game-with-kaplay
+
+
+Esta es una prueba de la librería Kaplay para hacer juegos en javascript.
+Seguí la primera parte de este tutorial: https://www.youtube.com/watch?v=LNgGdw5WT1s
+
+Link a la librería(que me encanta la página): https://kaplayjs.com/
+
+Repositorio de GitHub: https://github.com/kaplayjs/kaplay
+
+Esta prueba usa el CDN, asi que basta con el live server de VSCode para ejecutar el archivo
+html y jugar.
---
+++ script.js
@@ -0,0 +1,70 @@
+import kaplay from "https://unpkg.com/kaplay@3001/dist/kaplay.mjs";
+
+// start kaplay
+kaplay({
+  width: 1280,
+  height: 720,
+  background: '#e9967a',
+  debug: true,
+  burp: true,
+  // and more uselful opts, like burp is 
+});
+
+debug.log("Press (B) button to burp!");
+
+loadSprite("bean", "assets/bean.png");
+setGravity(2000);
+
+const player = add([
+  sprite("bean"),
+  pos(center()),
+  area(),
+  body(),
+  offscreen()
+]);
+
+player.onKeyPress("space", () => {
+  if (player.isGrounded()) player.jump()
+})
+
+player.onExitScreen(() => {
+  go("gameover");
+})
+
+scene("gameover", () => {
+  add([
+    text("Game Over!"),
+    pos(500, 300)
+  ]);
+})
+
+add([
+  rect(1280, 300),
+  pos(0, 500),
+  area(),
+  outline(3),
+  body({isStatic: true})
+]);
+
+let counter = 0;
+
+const counterUI = add([
+  text("0")
+]);
+
+loop(1, () => {
+  counter++;
+  counterUI.text = counter.toString();
+
+  const speeds = [300, 500, 800, 1000, 1500];
+  const currentSpeed = speeds[Math.floor(Math.random() * speeds.length)];
+
+  add([
+    rect(50, 50),
+    pos(1000, 500),
+    area(),
+    body(),
+    outline(3),
+    move(vec2(-1, 0), currentSpeed)
+  ])
+})
